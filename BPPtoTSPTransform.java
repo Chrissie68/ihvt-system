9ac2b632f1fc76f8043885c38a467d8ebc3ed473
Bram <b.d.sprokholt@outlook.com>
2024-05-30 13:52:31 +0200

    Update BPPtoTSPTransform
    Added comments to make code clearer

--- BPPtoTSPTransform.java
+++ BPPtoTSPTransform.java
@@ -5,6 +5,7 @@ public class BPPtoTSPTransform {
     public static int[][] locationTransform(int[] productsIDs) {
         int[][] locationAfter = new int[productsIDs.length][2];
         try {
+            //Building the query with inputs from input array.
             StringBuilder queryBuilder = new StringBuilder("SELECT StockItemID, BinLocation FROM stockitemholdings WHERE ");
             for (int i = 0; i < productsIDs.length; i++) {
                 queryBuilder.append("StockItemID = ").append(productsIDs[i]);
@@ -12,13 +13,15 @@ public class BPPtoTSPTransform {
                     queryBuilder.append(" OR ");
                 }
             }
-
+            //Executing generated query
             DefaultTableModel model = Database.executeSelectQuery(queryBuilder.toString());
             for (int i = 0; i < model.getRowCount(); i++) {
                 String binLocation = model.getValueAt(i, 1).toString();
+                //Defining the characters within the returned model.
                 char locationChar = binLocation.charAt(0);
                 char locationDigit = binLocation.charAt(1);
 
+                //Changing the value of the letter with a number to format it to the TSP algorithm
                 switch (locationChar) {
                     case 'A':
                         locationAfter[i][0] = 1;
@@ -38,11 +41,18 @@ public class BPPtoTSPTransform {
                     default:
                         break;
                 }
+                //Adding the number of the location to the new array which is being returned.
+                //To generate an output like this:
+                // int[][] = {
+                //  {1,1}
+                //  {5,3}
+                // }
                 locationAfter[i][1] = Character.getNumericValue(locationDigit);
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        //Returning array
         return locationAfter;
     }
 }
